cab8f2729d2272fec055e994628f4b19406cf91f
Ruchika Jangir <ruchika.jangir.met16@itbhu.ac.in>
2020-03-04 01:38:50 +0530

    Add files via upload

---
+++ Intel/GfxCPLBatchFiles/{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat
@@ -0,0 +1,4 @@
+@echo off
+TIMEOUT 1 /NOBREAK
+if exist "C:\WINDOWS\System32\DriverStore\FileRepository\ki130274.inf_amd64_bafd2440fa1ffdd6\igfxEM.exe" start "" "C:\WINDOWS\System32\DriverStore\FileRepository\ki130274.inf_amd64_bafd2440fa1ffdd6\igfxEM.exe"
+del /Q "C:\Intel\GfxCPLBatchFiles\{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat"
